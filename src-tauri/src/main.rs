56eb92fe7b863815a6dcd5e15d066727ec8cd426
Julius Kirsch <Julius@skirsch.de>
2024-02-22 18:15:27 +0100

    feat: add tokio runtime to main

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,14 +1,29 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use std::{thread, time::Duration};
 
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
-fn main() {
+/// `npm run tauri dev` to start the server
+
+
+#[tokio::main]
+async fn main() {
+    tokio::task::spawn(async move {
+        loop {
+            println!("Baum");
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+
+
+
 
 }
